fce547ab2ba817416a4ce8e4fb460af2a8cdf052
harshal1998 <harshalwankhede1998@gmail.com>
2021-06-22 11:34:55 +0530

    Initial commit

---
+++ project/app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.urls import path
+
+from . import views     # it means - 'from all import views'
+
+urlpatterns = [
+    path('',views.MainPage.as_view()),
+    path('apply',views.mainpage),
+    path('output',views.output),
+    # url(r'^down_file', views.down_file),
+    # path('transpose',views.transpose),
+    # url(r'^downfile_transpose', views.down_file_transpose),
+]
---
+++ project/app/views.py
@@ -0,0 +1,73 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+import pandas as pd
+import numpy as np
+
+
+class MainPage(TemplateView):
+    template_name = 'home.html'
+
+
+def mainpage(request):
+    file = pd.read_excel("Data Records.xlsx")
+    # print(file)
+    return render(request, "display.html", {"string": file})
+
+
+def output(request):
+    file = pd.read_excel("Data Records.xlsx")
+    # print(file)
+    var1 = request.POST['dropdown1']
+    # print(var1)
+    var2 = request.POST['dropdown2']
+    # print(var2)
+    data1 = file[var1]
+    data2 = file[var2]
+    global CrossTab
+    CrossTab = pd.crosstab(data1, data2, margins=True)
+    # print(CrossTab)
+    CrossTab.to_excel("Pivot_Data.xlsx")
+    b = CrossTab.to_html()
+
+    table = open("C:/Users/harsh/PycharmProjects/Assignment5-master/project/app/templates/output1.html", "w")
+    table.write(b)
+    table.close()
+
+    table = open("C:/Users/harsh/PycharmProjects/Assignment5-master/project/app/templates/output1.html", "a")
+    table.write('<form method="get" action="project/Pivot_Data.xlsx"><button type="submit">Download!</button></form>')
+    table.close()
+
+    # table = open("C:/Users/harsh/PycharmProjects/Assignment5-master/project/app/templates/output1.html", "a")
+    # table.write(r'<button onclick ="window.location.href=`/transpose`">Transpose</button>')
+    # table.close()
+    return render(request, "output1.html", {"string": b})
+
+# def down_file(request):
+#     with open('Pivot_Data.xlsx', 'rb') as model_excel1:
+#         result1 = model_excel1.read()
+#     response1 = HttpResponse(result1)
+#     response1['Content-Disposition'] = 'attachment; filename=Pivot_Table.xlsx'
+#     return response1
+#
+# def transpose(request):
+#     transpose=CrossTab.transpose()
+#     transpose.to_excel("Pivot_Data_Transpose.xlsx")
+#     c = transpose.to_html()
+#
+#     table = open("C:/Users/harsh/PycharmProjects/Assignment5-master/project/app/templates/output1.html", "w")
+#     table.write(c)
+#     table.close()
+#
+#     table = open("C:/Users/harsh/PycharmProjects/Assignment5-master/project/app/templates/output1.html", "a")
+#     table.write(r'<button onclick ="window.location.href=`/downfile_transpose`">Download Transpose File</button>')
+#     table.close()
+#
+#     return render(request, "output1.html", {"string": c})
+#
+# def down_file_transpose(request):
+#     with open('Pivot_Data_Transpose.xlsx', 'rb') as model_excel:
+#         result = model_excel.read()
+#     response = HttpResponse(result)
+#     response['Content-Disposition'] = 'attachment; filename=Pivot_Table_Transpose.xlsx'
+#     return response
